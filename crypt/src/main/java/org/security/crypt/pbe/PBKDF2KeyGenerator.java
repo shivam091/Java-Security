96570ca9611d37bc69d07c4e6a9471da3d84a82e
shivam <harshal.ladhe.91@live.co.uk>
2014-09-01 06:42:03 +0530

    Created PBKDF1KeyGenerator.java and PBKDF2KeyGenerator.java

---
+++ crypt/src/main/java/org/security/crypt/pbe/PBKDF2KeyGenerator.java
@@ -0,0 +1,48 @@
+package org.security.crypt.pbe;
+
+import org.bouncycastle.crypto.PBEParametersGenerator;
+import org.bouncycastle.crypto.generators.PKCS5S2ParametersGenerator;
+
+/**
+ * Implements the PBKDF2 key generation function defined in PKCS#5v2.
+ *
+ * @author shivam
+ * 
+ */
+public class PBKDF2KeyGenerator extends AbstractPKCSKeyGenerator {
+
+	/**
+	 * Creates a new instance that uses SHA1 hash for pseudorandom function to
+	 * generate derived keys.
+	 *
+	 * @param saltBytes
+	 *            Key derivation function salt bytes.
+	 */
+	public PBKDF2KeyGenerator(final byte[] saltBytes) {
+		this(saltBytes, DEFAULT_ITERATION_COUNT);
+	}
+
+	/**
+	 * Creates a new instance that uses SHA1 hash for pseudorandom function to
+	 * generate derived keys.
+	 *
+	 * @param saltBytes
+	 *            Key derivation function salt bytes.
+	 * @param iterations
+	 *            Key derivation function iteration count.
+	 */
+	public PBKDF2KeyGenerator(final byte[] saltBytes, final int iterations) {
+		this.salt = saltBytes;
+		setIterationCount(iterations);
+	}
+
+	/** {@inheritDoc} */
+	protected PBEParametersGenerator newParamGenerator() {
+		return new PKCS5S2ParametersGenerator();
+	}
+
+	/** {@inheritDoc} */
+	protected byte[] toBytes(final char[] password) {
+		return PBEParametersGenerator.PKCS5PasswordToBytes(password);
+	}
+}
