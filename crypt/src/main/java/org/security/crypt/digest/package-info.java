65a040873c94c811bb8392507fa1dce18e547b8a
shivam <harshal.ladhe.91@live.co.uk>
2014-09-01 06:35:44 +0530

    Created package-info.java

---
+++ crypt/src/main/java/org/security/crypt/digest/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * <code>This package contains classes to implement MEssage Digest algorithms viz. MD, RipeMD, SHA, Tiger, Whirlpool, etc.</code>
+ * 
+ * @author shivam
+ *
+ */
+package org.security.crypt.digest;
