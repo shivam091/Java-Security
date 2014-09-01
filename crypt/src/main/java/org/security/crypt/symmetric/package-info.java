d85217a93205c52e850de927b6f8f27789bc3d42
shivam <harshal.ladhe.91@live.co.uk>
2014-09-01 06:53:03 +0530

    Created package-info.java

---
+++ crypt/src/main/java/org/security/crypt/symmetric/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * <code>This package contains classes to implement symmetric algorithms viz, AES, DES, DESede, RC, CAST, Rijndael Twofish, etc.</code>
+ * 
+ * @author shivam
+ *
+ */
+package org.security.crypt.symmetric;
