5ef25b347495696bd025b208c00da50be8bc7329
shivam <harshal.ladhe.91@live.co.uk>
2014-09-01 06:31:49 +0530

    Created RipeMD256.java and RipeMD320.java

---
+++ crypt/src/main/java/org/security/crypt/digest/RipeMD256.java
@@ -0,0 +1,49 @@
+package org.security.crypt.digest;
+
+import java.security.SecureRandom;
+import org.bouncycastle.crypto.digests.RIPEMD256Digest;
+
+/**
+ * <p>
+ * <code>RipeMD256</code> contains functions for hashing data using the
+ * RipeMD256 algorithm. This algorithm outputs a 256 bit hash, but offers the
+ * same level of security as RipeMD128.
+ * </p>
+ *
+ * @author shivam
+ * 
+ */
+
+public class RipeMD256 extends DigestAlgorithm {
+
+	/** Creates an uninitialized instance of an RipeMD256 digest. */
+	public RipeMD256() {
+		super(new RIPEMD256Digest());
+	}
+
+	/**
+	 * Creates a new RipeMD256 digest that may optionally be initialized with
+	 * random data.
+	 *
+	 * @param randomize
+	 *            True to randomize initial state of digest, false otherwise.
+	 */
+	public RipeMD256(final boolean randomize) {
+		super(new RIPEMD256Digest());
+		if (randomize) {
+			setRandomProvider(new SecureRandom());
+			setSalt(getRandomSalt());
+		}
+	}
+
+	/**
+	 * Creates a new RipeMD256 digest and initializes it with the given salt.
+	 *
+	 * @param salt
+	 *            Salt data used to initialize digest computation.
+	 */
+	public RipeMD256(final byte[] salt) {
+		super(new RIPEMD256Digest());
+		setSalt(salt);
+	}
+}
